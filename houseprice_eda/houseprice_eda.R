2d70fcc1208ef3986f511ec3f73fc95b0bc7962b
HyeongGyu Choi <imhgchoi@naver.com>
2018-01-13 14:57:03 +0900

    commit

--- houseprice_eda/houseprice_eda.R
+++ houseprice_eda/houseprice_eda.R
@@ -1,3 +1,61 @@
 train_dev <- read.csv("D:/rawDataFiles/housePrice_train.csv",header=T)
 test <- read.csv("D:/rawDataFiles/housePrice_test.csv",header=T)
 
+
+train_dev$Alley <- as.character(train_dev$Alley)
+train_dev$Alley[is.na(train_dev$Alley)] <- 'NoAlley'
+train_dev$Alley <- as.factor(train_dev$Alley)
+
+train_dev$BsmtQual <- as.character(train_dev$BsmtQual)
+train_dev$BsmtQual[is.na(train_dev$BsmtQual)] <- 'NoBsmt'
+train_dev$BsmtQual <- as.factor(train_dev$BsmtQual)
+
+train_dev$BsmtCond <- as.character(train_dev$BsmtCond)
+train_dev$BsmtCond[is.na(train_dev$BsmtCond)] <- 'NoBsmt'
+train_dev$BsmtCond <- as.factor(train_dev$BsmtCond)
+
+train_dev$BsmtExposure <- as.character(train_dev$BsmtExposure)
+train_dev$BsmtExposure[is.na(train_dev$BsmtExposure)] <- 'NoBsmt'
+train_dev$BsmtExposure <- as.factor(train_dev$BsmtExposure)
+
+train_dev$BsmtFinType1 <- as.character(train_dev$BsmtFinType1)
+train_dev$BsmtFinType1[is.na(train_dev$BsmtFinType1)] <- 'NoBsmt'
+train_dev$BsmtFinType1 <- as.factor(train_dev$BsmtFinType1)
+
+train_dev$BsmtFinType2 <- as.character(train_dev$BsmtFinType2)
+train_dev$BsmtFinType2[is.na(train_dev$BsmtFinType2)] <- 'NoBsmt'
+train_dev$BsmtFinType2 <- as.factor(train_dev$BsmtFinType2)
+
+train_dev$FireplaceQu <- as.character(train_dev$FireplaceQu)
+train_dev$FireplaceQu[is.na(train_dev$FireplaceQu)] <- 'NoFireplace'
+train_dev$FireplaceQu <- as.factor(train_dev$FireplaceQu)
+
+train_dev$GarageType <- as.character(train_dev$GarageType)
+train_dev$GarageType[is.na(train_dev$GarageType)] <- 'NoGarage'
+train_dev$GarageType <- as.factor(train_dev$GarageType)
+
+train_dev$GarageFinish <- as.character(train_dev$GarageFinish)
+train_dev$GarageFinish[is.na(train_dev$GarageFinish)] <- 'NoGarage'
+train_dev$GarageFinish <- as.factor(train_dev$GarageFinish)
+
+train_dev$GarageQual <- as.character(train_dev$GarageQual)
+train_dev$GarageQual[is.na(train_dev$GarageQual)] <- 'NoGarage'
+train_dev$GarageQual <- as.factor(train_dev$GarageQual)
+
+train_dev$GarageCond <- as.character(train_dev$GarageCond)
+train_dev$GarageCond[is.na(train_dev$GarageCond)] <- 'NoGarage'
+train_dev$GarageCond <- as.factor(train_dev$GarageCond)
+
+train_dev$PoolQC <- as.character(train_dev$PoolQC)
+train_dev$PoolQC[is.na(train_dev$PoolQC)] <- 'NoPool'
+train_dev$PoolQC <- as.factor(train_dev$PoolQC)
+
+train_dev$Fence <- as.character(train_dev$Fence)
+train_dev$Fence[is.na(train_dev$Fence)] <- 'NoFence'
+train_dev$Fence <- as.factor(train_dev$Fence)
+
+train_dev$MiscFeature <- as.character(train_dev$MiscFeature)
+train_dev$MiscFeature[is.na(train_dev$MiscFeature)] <- 'None'
+train_dev$MiscFeature <- as.factor(train_dev$MiscFeature)
+
+
--- preprocessing.py
+++ preprocessing.py
@@ -5,8 +5,8 @@ import numpy as np
 def data_import():
     train_dev = pd.read_csv("D:/rawDataFiles/housePrice_train.csv")
     test = pd.read_csv("D:/rawDataFiles/housePrice_test.csv")
-    print(train_dev.describe())
-    print(test.describe())
+    print('train_dev set shape : ' , train_dev.shape)  # train_dev set shape :  (1460, 81)
+    print('test set shape : ' , test.shape)            # test set shape :  (1459, 80)
 
     return train_dev, test
 
@@ -23,5 +23,19 @@ def na_check(train_dev, test):
     # https://storage.googleapis.com/kaggle-competitions-data/kaggle/5407/data_description.txt?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1516070880&Signature=aRKYNWh%2FHIHQNWPpczNU3G1syEYPp%2F8Q1d0ga4GqjP3NY%2F6ZKAyImuU1flxo7Rx8cCJeeYvYGdPetCIiC1%2F9KuFedaFx6vOhLTuqq1tEcJU%2BDAc%2BG1Q6ODv86BjiqaEtCSQx%2BEqVIhDvQA9kQwXWSKbR%2Fx7BW4NdUXh0PkkX9VXs0hQxSPPubs2oiEZzuaP%2F3FQlABH46WidivszV4cYCr8Z8dscmJQ4oRh59RK0qE27AIkkPwvq8FfR5oHQumILtN04LSxpOZlsjzxv%2BCN8HE8GkPyzwUhnVGaWSrPkGp3DUpb8Ct2EkT0x7wUF92FVEw61QVTWVkn6tXsQRSre%2Fg%3D%3D
 
     for dataset in [train_dev, test] :
-        dataset.Alley = train_dev.Alley.replace(np.nan, 'NoAlley')
-        dataset.
+        dataset.Alley = dataset.Alley.replace(np.nan, 'NoAlley')
+        dataset.BsmtQual = dataset.BsmtQual.replace(np.nan, 'NoBsmt')
+        dataset.BsmtCond = dataset.BsmtCond.replace(np.nan, 'NoBsmt')
+        dataset.BsmtExposure = dataset.BsmtExposure.replace(np.nan, 'NoBsmt')
+        dataset.BsmtFinType1 = dataset.BsmtFinType1.replace(np.nan, 'NoBsmt')
+        dataset.BsmtFinType2 = dataset.BsmtFinType2.replace(np.nan, 'NoBsmt')
+        dataset.FireplaceQu = dataset.FireplaceQu.replace(np.nan, 'NoFireplace')
+        dataset.GarageType = dataset.GarageType.replace(np.nan, 'NoGarage')
+        dataset.GarageFinish = dataset.GarageFinish.replace(np.nan, 'NoGarage')
+        dataset.GarageQual = dataset.GarageQual.replace(np.nan, 'NoGarage')
+        dataset.GarageCond = dataset.GarageCond.replace(np.nan, 'NoGarage')
+        dataset.PoolQC = dataset.PoolQC.replace(np.nan, 'NoPool')
+        dataset.Fence = dataset.Fence.replace(np.nan, 'NoFence')
+        dataset.MiscFeature = dataset.MiscFeature.replace(np.nan, 'None')
+
+    print(train_dev.isnull())
